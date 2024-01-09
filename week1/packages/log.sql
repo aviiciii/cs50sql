b61c553a269a13de306ed251cc9ac1b242f5c5a6
aviiciii <laavesh1@gmail.com>
2024-01-09 22:32:24 +0530

    format

--- week1/packages/log.sql
+++ week1/packages/log.sql
@@ -2,32 +2,32 @@
 -- *** The Lost Letter ***
 
 -- find the id of 900 Somerville Avenue
-select id from addresses where address = '900 Somerville Avenue';
--- find the packages sent from 900 Somerville Avenue (id = 432) and find the package id of the Congratualatory Note
-select * from packages where from_address_id = 432;
+SELECT id FROM addresses WHERE address = '900 Somerville Avenue';
+-- find the packages sent FROM 900 Somerville Avenue (id = 432) and find the package id of the Congratualatory Note
+SELECT * FROM packages WHERE FROM_address_id = 432;
 -- find the scans for the Congratualatory Note package (id = 384)
-select * from scans where package_id = 384;
+SELECT * FROM scans WHERE package_id = 384;
 -- find the address that the package was dropped (id= 854)
-select * from addresses where id = 854;
+SELECT * FROM addresses WHERE id = 854;
 
 -- *** The Devious Delivery ***
 
--- find the id of packages with no from address
-select * from packages where from_address_id is null;
+-- find the id of packages with no FROM address
+SELECT * FROM packages WHERE FROM_address_id IS NULL;
 -- find the scans for the package of the devious package (id = 5098)
-select * from scans where package_id = 5098;
+SELECT * FROM scans WHERE package_id = 5098;
 -- find the address that the package was dropped (id= 348)
-select * from addresses where id = 348;
+SELECT * FROM addresses WHERE id = 348;
 
 -- *** The Forgotten Gift ***
 
--- find the packages from 109 Tileston Street
-select * from packages where from_address_id = (
-    select id from addresses where address = "109 Tileston Street"
+-- find the packages FROM 109 Tileston Street
+SELECT * FROM packages WHERE FROM_address_id = (
+    SELECT id FROM addresses WHERE address = "109 Tileston Street"
 );
 
 -- find the scans for the package of the forgotten gift (id = 9523)
-select * from scans where package_id = 9523;
+SELECT * FROM scans WHERE package_id = 9523;
 
 -- find the driver that has the package (id= 17)
-select * from drivers where id = 17;
+SELECT * FROM drivers WHERE id = 17;
